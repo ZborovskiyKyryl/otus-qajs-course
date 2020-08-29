7426259267b11c2b4e3a1723f41f7f1168ad5cbc
Kyryl Zborovskiy <speak.zborovskiy@gmail.com>
2020-08-29 19:48:20 +0300

    parm test homework2.1

--- param_test.js
+++ param_test.js
@@ -1,9 +1,9 @@
 Feature('paramTest');
 
-// Define data table inside a test or load from another module
 
-let accounts = new DataTable(['toDofiled']); //
-accounts.add(['davert']); // adding records to a table
+
+let accounts = new DataTable(['toDofiled']); 
+accounts.add(['davert']); 
 accounts.add(['admin']);
 accounts.add(['chachacha']);
 accounts.add(['aleluya']);
@@ -12,15 +12,11 @@ accounts.add(['@@(@(@(']);
 accounts.add(['JIU#UH##']);
 accounts.add(['czxczxc76']);
 
-// You can skip some data. But add them to report as skipped (just like with usual scenarios):
-//accounts.xadd(['admin', '23456'])
 
-// Pass dataTable to Data()
-// Use special param `current` to get current data set
 Data(accounts).Scenario('ToDo', (I, current) => {
   I.amOnPage('http://todomvc.com/examples/emberjs/');
-  I.fillField('What needs to be done?', current.toDofiled); // current is reserved!
-  //I.fillField('Password', current.password);
+  I.fillField('What needs to be done?', current.toDofiled); 
+  
   I.pressKey('Enter');
   I.see(current.toDofiled);
 });
